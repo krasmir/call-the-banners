a6e18adf29392974f29ad413fc1c29de86636ad5
Mirosław Kraszewski <kraszewskimiroslaw@gmail.com>
2022-10-01 01:20:21 +0200

    Refactor userArmySlice to one deleteUnit reducer

--- src/store/userArmy/userArmySlice.ts
+++ src/store/userArmy/userArmySlice.ts
@@ -1,17 +1,27 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { NCU, ArmyAttachment, ArmyCombatUnit } from "../../types";
+import { ArmyAttachment, ArmyCombatUnit, ArmyNCU } from "../../types";
 export interface UserArmyState {
     [key: string]: Army;
 }
 
 export interface Army {
-    units: ArmyCombatUnit[];
-    ncus: NCU[];
+    combatUnits: ArmyCombatUnit[];
+    ncus: ArmyNCU[];
     attachments: ArmyAttachment[];
 }
 
+type Unit = ArmyCombatUnit & ArmyAttachment & ArmyNCU;
+
+export type UnitType = "combatUnits" | "ncus" | "attachments";
+
+interface DeletePayload {
+    unitType: UnitType;
+    id: string;
+    currentFaction: string;
+}
+
 const emptyState: Army = {
-    units: [],
+    combatUnits: [],
     ncus: [],
     attachments: [],
 };
@@ -38,47 +48,36 @@ export const userArmySlice = createSlice({
     name: "userArmy",
     initialState,
     reducers: {
-        addUnit: (state, action: PayloadAction<ArmyCombatUnit>) => {
-            state.Lannister?.units?.push(action.payload);
-        },
-        deleteUnit: (state, action: PayloadAction<string>) => {
-            const newUnits = state.Lannister?.units?.filter(
-                (unit) => unit.uuid !== action.payload
+        addCombatUnit: (state, action: PayloadAction<ArmyCombatUnit>) => {
+            state[action.payload.currentFaction].combatUnits?.push(
+                action.payload
             );
-            const newAttachments = state.Lannister?.attachments?.filter(
-                (unit) => unit.attachedTo !== action.payload
-            );
-            state.Lannister.units = newUnits;
-            state.Lannister.attachments = newAttachments;
         },
-        addNCU: (state, action: PayloadAction<NCU>) => {
-            state.Lannister?.ncus?.push(action.payload);
+        deleteUnit: (state, action: PayloadAction<DeletePayload>) => {
+            const { id, currentFaction, unitType } = action.payload;
+            const newUnits: Unit[] = (
+                state[currentFaction][unitType] as Unit[]
+            ).filter((unit: Unit) => unit.uuid !== id);
+            if (unitType === "combatUnits") {
+                const newAttachments = state[currentFaction].attachments.filter(
+                    (unit) => unit.attachedTo !== id
+                );
+                state[currentFaction].attachments = newAttachments;
+            }
+            state[currentFaction][unitType] = newUnits;
         },
-        deleteNCU: (state, action: PayloadAction<string>) => {
-            const newNcus = state.Lannister?.ncus?.filter(
-                (unit) => unit.id !== action.payload
-            );
-            state.Lannister.ncus = newNcus;
+        addNCU: (state, action: PayloadAction<ArmyNCU>) => {
+            state[action.payload.currentFaction].ncus?.push(action.payload);
         },
         addAttachment: (state, action: PayloadAction<ArmyAttachment>) => {
-            state.Lannister?.attachments?.push(action.payload);
-        },
-        deleteAttachment: (state, action: PayloadAction<string>) => {
-            const newAttachments = state.Lannister?.attachments?.filter(
-                (unit) => unit.uuid !== action.payload
+            state[action.payload.currentFaction].attachments?.push(
+                action.payload
             );
-            state.Lannister.attachments = newAttachments;
         },
     },
 });
 
-export const {
-    addUnit,
-    deleteUnit,
-    addNCU,
-    deleteNCU,
-    addAttachment,
-    deleteAttachment,
-} = userArmySlice.actions;
+export const { addCombatUnit, deleteUnit, addNCU, addAttachment } =
+    userArmySlice.actions;
 
 export default userArmySlice.reducer;
