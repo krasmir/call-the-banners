cb593129ed7632c4b49dee4f73633725c487ad1a
Mirosław Kraszewski <kraszewskimiroslaw@gmail.com>
2022-10-08 23:57:26 +0200

    Add crest and commander to tactics card, improve styling

--- src/TacticsCard.tsx
+++ src/TacticsCard.tsx
@@ -10,26 +10,49 @@ const Card = styled.div`
     background-repeat: no-repeat;
     background-size: cover;
     border-radius: 4px;
-    /* border: ${(props) => props.theme.colors.secondary} solid 1px; */
 
     padding: 0 5px;
     text-align: center;
-    font-size: 6px;
-    /* letter-spacing: 0.6px; */
+    font-size: 5px;
+
     &:hover {
         transform: scale(4) translate(-35%, 20%);
         z-index: 5;
     }
 `;
+
+const TitleDiv = styled.div`
+    height: 16%;
+    display: flex;
+    margin: 2%;
+`;
+
+const CrestImg = styled.img`
+    height: 100%;
+`;
+
 const H3 = styled.h3`
-    letter-spacing: 2px;
+    letter-spacing: 1px;
     text-transform: uppercase;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
     font-weight: bold;
+    font-size: 5px;
+    font-family: "Courier New", Courier, monospace;
+    color: white;
     margin: 2px;
+`;
+
+const Deck = styled(H3)`
+    font-size: 4px;
+    margin: 0 auto;
 `;
 
 const H4 = styled.h4`
     font-weight: bold;
+    font-size: 5px;
     margin: 2px;
 `;
 
@@ -38,8 +61,9 @@ const CardText = styled.div`
     height: 70%;
     position: absolute;
     top: 26%;
-    padding: 5%;
-    font-size: 4px;
+    padding: 0 5%;
+    font-size: 5px;
+    font-family: Georgia, "Times New Roman", Times, serif;
     color: black;
     border: 1px inset grey;
     background-image: url("./Bg2.jpg");
@@ -54,7 +78,7 @@ const IMG = styled.img`
 `;
 
 function TacticsCard({ card }: { card: TacticCard }): JSX.Element {
-    const { name, text } = card;
+    const { name, text, deck } = card;
     let firstTrigger: string = "";
     let secondTrigger: string | undefined;
 
@@ -118,7 +142,14 @@ function TacticsCard({ card }: { card: TacticCard }): JSX.Element {
                 )`,
             }}
         >
-            <H3>{name}</H3>
+            <TitleDiv>
+                <CrestImg
+                    src={`./Crest${card.faction.replace(/[' ]/g, "")}.webp`}
+                />
+                <H3>{name}</H3>
+            </TitleDiv>
+            {!deck.includes("Basic Deck") && <Deck>{deck}</Deck>}
+
             <CardText>
                 <H4>{processText(firstTrigger)}</H4>
                 <p>{processText(newText[0])}</p>
