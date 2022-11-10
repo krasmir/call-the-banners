ce49e0985fa190a7d984b1af057f54bd665c95a8
Mirosław Kraszewski <kraszewskimiroslaw@gmail.com>
2022-11-10 22:54:34 +0100

    Create MoraleVertIcon

---
+++ src/MoraleVertIcon.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components/macro";
+
+const IconDiv = styled.div`
+    height: 90px;
+    position: relative;
+    top: -10px;
+    left: 4px;
+`;
+
+const IMG = styled.img`
+    height: 100%;
+    position: absolute;
+    z-index: 2;
+`;
+
+const ValueDiv = styled.div`
+    position: absolute;
+    top: 52px;
+    left: 6px;
+    transform: translateY(-50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 19px;
+    font-weight: bold;
+    width: 30px;
+    height: 30px;
+    color: #cdcdcd;
+    background-image: url("./Cost.webp");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
+
+function MoraleVertIcon(): JSX.Element {
+    return (
+        <IconDiv>
+            <IMG alt="Morale" src="./IconMorale.png" title="Morale" />
+            <ValueDiv>{5}</ValueDiv>
+        </IconDiv>
+    );
+}
+
+export default MoraleVertIcon;
