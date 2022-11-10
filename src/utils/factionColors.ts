448c841a18a5eca5087430fbe89b21ae797a3b14
Mirosław Kraszewski <kraszewskimiroslaw@gmail.com>
2022-11-10 22:23:25 +0100

    Create factionColors

---
+++ src/utils/factionColors.ts
@@ -0,0 +1,23 @@
+export interface FactionColors {
+    Lannister: string;
+    Stark: string;
+    "Free Folk": string;
+    Neutral: string;
+    "Night's Watch": string;
+    Baratheon: string;
+    Targaryen: string;
+    Greyjoy: string;
+    Martell: string;
+}
+
+export const factionColors: FactionColors = {
+    Lannister: "#8e201b",
+    Stark: "#366a8a",
+    "Free Folk": "#483e33",
+    Neutral: "#7e2b12",
+    "Night's Watch": "black",
+    Baratheon: "#a64f28",
+    Targaryen: "#a34450",
+    Greyjoy: "#387071",
+    Martell: "#a05f33",
+};
